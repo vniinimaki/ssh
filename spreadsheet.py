0dbde32d033cc2344606f8f3f77ccf776f45e85d
sromano87 <siromano@unisa.it>
2024-10-11 10:00:17 +0200

    Add source files

---
+++ spreadsheet.py
@@ -0,0 +1,14 @@
+class SpreadSheet:
+
+    def __init__(self):
+        self._cells = {}
+        self._evaluating = set()
+
+    def set(self, cell: str, value: str) -> None:
+        self._cells[cell] = value
+
+    def get(self, cell: str) -> str:
+        return self._cells.get(cell, '')
+
+    def evaluate(self, cell: str) -> str:
+        pass
---
+++ test/testspreadsheet.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from spreadsheet import SpreadSheet
+
+
+class TestSpreadsheet(TestCase):
+
+    def test_something(self):
+        pass
