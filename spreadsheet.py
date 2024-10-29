974221e7e250aa51d0b09e8c877d586350d571df
Verneri <vniinima18@student.oulu.fi>
2024-10-29 17:56:15 +0200

    us4 test3

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,37 +1,40 @@
 
 class SpreadSheet:
-
     def __init__(self):
         self._cells = {}
+        self._evaluating = set()  # To track cells currently being evaluated
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
 
     def evaluate(self, cell: str):
+        if cell in self._evaluating:
+            return "#Circular"
+        self._evaluating.add(cell)
+        
         value = self._cells.get(cell, "")
         if value.startswith("="):
             if value[1:].startswith("'") and value[-1] == "'":
-                return value[2:-1]
+                result = value[2:-1]
+            else:
+                try:
+                    # Check if it's a reference to another cell
+                    if value[1].isalpha():
+                        referenced_cell = value[1:]
+                        referenced_value = self._cells.get(referenced_cell, "")
+                        result = self.evaluate(referenced_cell)
+                    else:
+                        result = int(value[1:])
+                except ValueError:
+                    result = "#Error"
+        elif value.startswith("'") and value.endswith("'"):
+            result = value[1:-1]
+        else:
             try:
-                # Check if it's a reference to another cell
-                if value[1].isalpha():
-                    referenced_value = self._cells.get(value[1:])
-                    if referenced_value.isdigit():
-                        return int(referenced_value)
-                    elif referenced_value.startswith("'") and referenced_value.endswith("'"):
-                        return referenced_value[1:-1]
-                    elif referenced_value.startswith("="):
-                        return self.evaluate(value[1:])
-                    elif '.' in referenced_value:
-                        return "#Error"
-                    return referenced_value
-                return int(value[1:])
+                result = int(value)
             except ValueError:
-                return "#Error"
-        if value.startswith("'") and value.endswith("'"):
-            return value[1:-1]
-        try:
-            return int(value)
-        except ValueError:
-            return "#Error"
+                result = "#Error"
+        
+        self._evaluating.remove(cell)
+        return result
 
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -42,6 +42,12 @@ class TestSpreadSheet(TestCase):
         spreadsheet.set("A1","=B1")
         spreadsheet.set("B1", "42.5")
         self.assertEqual("#Error", spreadsheet.evaluate("A1"))
+    def test_circular_reference(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1","=B1")
+        spreadsheet.set("B1", "=A1")
+        self.assertEqual("#Circular", spreadsheet.evaluate("A1"))
+
 
 
 
