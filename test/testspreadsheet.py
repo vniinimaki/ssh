75c579e8fb2398aa746d84170271f82086856a4a
Verneri <vniinima18@student.oulu.fi>
2024-10-29 17:41:29 +0200

    us3 test3

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -28,6 +28,10 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1","=1")
         self.assertEqual(1, spreadsheet.evaluate("A1"))
+    def test_evaluate_simple_invalid_formulas(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1","='Apple")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
 
 
 
