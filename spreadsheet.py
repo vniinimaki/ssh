6c404f544f8ab8556a8f50de8e2449dff33a10a9
Verneri <vniinima18@student.oulu.fi>
2024-10-29 17:22:48 +0200

    us1 test1

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,14 +1,12 @@
+
 class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
-        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
 
-    def get(self, cell: str) -> str:
-        return self._cells.get(cell, '')
+    def evaluate(self, cell: str) -> int:
+        return int(self._cells[cell])
 
-    def evaluate(self, cell: str) -> int | str:
-        pass
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -4,5 +4,7 @@ from spreadsheet import SpreadSheet
 
 class TestSpreadSheet(TestCase):
 
-    def test_something(self):
-        pass
+    def test_evaluate_valid_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1","1")
+        self.assertEqual(1, spreadsheet.evaluate("A1"))
