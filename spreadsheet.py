caec4a1772aff3c19efea1fef2c8f13d62cd6fbb
Verneri <vniinima18@student.oulu.fi>
2024-10-29 17:36:21 +0200

    us2 test2

--- spreadsheet.py
+++ spreadsheet.py
@@ -8,8 +8,11 @@ class SpreadSheet:
         self._cells[cell] = value
 
     def evaluate(self, cell: str):
+        value = self._cells.get(cell, "")
+        if value.startswith("'") and value.endswith("'"):
+            return value[1:-1]
         try:
-            return int(self._cells[cell])
+            return int(value)
         except ValueError:
             return "#Error"
 
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -12,4 +12,14 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1","1.5")
         self.assertEqual("#Error", spreadsheet.evaluate("A1"))
+    def test_evaluate_valid_string(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1","'Apple'")
+        self.assertEqual("Apple", spreadsheet.evaluate("A1"))
+    def test_evaluate_non_valid_string(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1","'Apple")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
+
+
 
